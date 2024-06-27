808c8fff89853426eed4aaf07d6e280dab4f2d47
DiegoAvellaneda <48260188+diegoavellanedat17@users.noreply.github.com>
2024-06-26 22:14:12 -0500

    fix: route for the styles home apos

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
 "use client";
 
 import Link from "next/link";
-import styles from "../src/styles/Home.module.css";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <header className={styles.header}>
         <h1>Welcome to My Pet</h1>
-        <p>Your pet's management system.</p>
+        <p>Your pet&apos;s management system.</p>
         <Link href="/login">
           <button className={styles.button}>Login</button>
         </Link>
@@ -16,9 +16,9 @@ export default function Home() {
       <section className={styles.about}>
         <h2>About</h2>
         <p>
-          My Pet helps you manage your pet's vaccination dates and other
-          important events. Keep all your pet's information in one place and
-          never miss an important date again.
+          My Pet helps you manage your pet&apos;s vaccination dates and other
+          important events. Keep all your pet&apos;s information in one place
+          and never miss an important date again.
         </p>
       </section>
     </div>
