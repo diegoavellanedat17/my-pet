e2290be4977a2d884e2b4e44d1533d2d37369f92
DiegoAvellaneda <48260188+diegoavellanedat17@users.noreply.github.com>
2024-06-30 22:48:59 -0500

    fix: new workflow for ci/cd

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    COGNITO_USER_POOL_ID: process.env.COGNITO_USER_POOL_ID,
-    COGNITO_USER_POOL_CLIENT_ID: process.env.COGNITO_USER_POOL_CLIENT_ID,
-    COGNITO_IDENTITY_POOL_ID: process.env.COGNITO_IDENTITY_POOL_ID,
+    NEXT_PUBLIC_USER_POOL_ID: process.env.NEXT_PUBLIC_USER_POOL_ID,
+    NEXT_PUBLIC_USER_POOL_CLIENT_ID:
+      process.env.NEXT_PUBLIC_USER_POOL_CLIENT_ID,
+    NEXT_PUBLIC_IDENTITY_POOL_ID: process.env.NEXT_PUBLIC_IDENTITY_POOL_ID,
   },
 };
 
