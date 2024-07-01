c7a91ca139849001319faf7df2b3dcb7c53d97c5
DiegoAvellaneda <48260188+diegoavellanedat17@users.noreply.github.com>
2024-06-30 21:16:31 -0500

    changed the name for the envs vvariables

--- src/aws-exports.ts
+++ src/aws-exports.ts
@@ -1,11 +1,10 @@
 const awsConfig = {
   Auth: {
     Cognito: {
-      userPoolId: process.env.NEXT_PUBLIC_USER_POOL_ID || 'default-pool-id',
-      userPoolClientId:
-        process.env.NEXT_PUBLIC_USER_POOL_CLIENT_ID || 'client-id',
+      userPoolId: process.env.COGNITO_USER_POOL_ID || 'default-pool-id',
+      userPoolClientId: process.env.COGNITO_USER_POOL_CLIENT_ID || 'client-id',
       identityPoolId:
-        process.env.NEXT_PUBLIC_IDENTITY_POOL_ID || 'identity-pool-id',
+        process.env.COGNITO_IDENTITY_POOL_ID || 'identity-pool-id',
     },
   },
 };
